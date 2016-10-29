bf7558e812549ea2643adf0789db940a05aeb7f0
Konstantin <Konstantin@Konstantin-PC>
2016-10-29 16:34:51 +0300

    Solutions of 2_DataTypes_Variables_Operators_Memory

---
+++ 2_DataTypes_Variables_Operators_Memory/src/task1A.java
@@ -0,0 +1,14 @@
+
+
+public class task1A {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 10;
+        int z = ++x * y--;
+
+        System.out.println("The result is: " + z);
+    }
+
+}
